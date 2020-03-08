5b06ef1e8e65c97a75820e35de7a7f0ca54b613c
auxfuse <aux.fuse@yahoo.com>
2020-03-08 22:19:00 +0000

    Added localhost & heroku app link to allowed hosts in settings.py file

--- Milestone4/settings.py
+++ Milestone4/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'php-barbell.herokuapp.com'
+]
 
 
 # Application definition
