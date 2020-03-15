45c1093235928e9fa5beb359578dc10a4d26050b
auxfuse <aux.fuse@yahoo.com>
2020-03-15 18:05:31 +0000

    accounts app created with default template routes in place

--- Milestone4/settings.py
+++ Milestone4/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'forum.apps.ForumConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
