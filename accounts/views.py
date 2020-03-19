239caf7fe94ff1af0e524da3466860fbf262c8df
auxfuse <aux.fuse@yahoo.com>
2020-03-19 22:20:02 +0000

    registration function created and tested

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User, auth
 
 
 # Create your views here.
@@ -9,8 +10,40 @@ def register(request):
     }
 
     if request.method == 'POST':
-        messages.error(request, 'User already exists')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check Password match
+        if password == password2:
+            # Check if username is unique
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already used.')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'This email is already coupled to an '
+                                        'account.')
+                return redirect('register')
+            else:
+                # Register User in Users collection & redirect to Home page.
+                user = User.objects.create_user(
+                    first_name=first_name,
+                    last_name=last_name,
+                    username=username,
+                    email=email,
+                    password=password
+                )
+                user.save()
+                auth.login(request, user)
+                messages.success(request, 'You are now registered & logged in.')
+                return redirect('index')
+        else:
+            messages.error(request, 'Passwords did not match, try again.')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html', context)
 
