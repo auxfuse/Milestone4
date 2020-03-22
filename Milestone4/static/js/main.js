817d84914f7595c7f6b47fd34bef600880277d45
auxfuse <aux.fuse@yahoo.com>
2020-03-22 20:03:15 +0000

    Moved typewriter function to own js file and rendered in index template via block script tag

--- Milestone4/static/js/main.js
+++ Milestone4/static/js/main.js
@@ -2,36 +2,3 @@
 setTimeout(function() {
     $('#msg-alert').fadeOut('slow');
 }, 3000);
-
-// Typewriter
-const words = [
-    'Form',
-    'Technique',
-    'Strength',
-    'Stability',
-    'Explosivity',
-    'Core',
-    'Character'
-];
-let count = 0;
-let currentWord = 0;
-let currentText = '';
-let letter = '';
-let home = 'https://php-barbell.herokuapp.com/';
-let localHome = 'http://127.0.0.1:8000/';
-
-if (location.href === home || location.href === localHome)
-    (function type(){
-        if(count === words.length) {
-          count = 0;
-        }
-        currentText = words[count];
-        letter = currentText.slice(0, ++currentWord);
-
-        document.querySelector('.typewriter-txt').textContent = letter;
-        if(letter.length === currentText.length){
-            count++;
-            currentWord = 0;
-        }
-        setTimeout(type, 340);
-    }());
---
+++ Milestone4/static/js/typewriter.js
@@ -0,0 +1,29 @@
+// Typewriter
+const words = [
+    'Form',
+    'Technique',
+    'Strength',
+    'Stability',
+    'Explosivity',
+    'Core',
+    'Character'
+];
+let count = 0;
+let currentWord = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+    if(count === words.length) {
+      count = 0;
+    }
+    currentText = words[count];
+    letter = currentText.slice(0, ++currentWord);
+
+    document.querySelector('.typewriter-txt').textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        currentWord = 0;
+    }
+    setTimeout(type, 340);
+}());
