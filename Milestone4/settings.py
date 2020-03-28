fce5a3f18a7aba78c25a06e167ea379a315d8c98
auxfuse <aux.fuse@yahoo.com>
2020-03-28 14:37:34 +0000

    Testing Database heroku connection

--- Milestone4/settings.py
+++ Milestone4/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'Milestone4.wsgi.application'
 DB_URL = os.environ.get("DB_URL")
 if DB_URL in os.environ:
     DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DB_URL"))
+        "default": dj_database_url.parse(DB_URL)
     }
 else:
     print("Localhost sqlite3 being used.")
