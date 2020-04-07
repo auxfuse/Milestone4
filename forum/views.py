024f205002799c8d52fc6f8f71f99d8e353c427a
auxfuse <aux.fuse@yahoo.com>
2020-04-07 23:03:36 +0100

    Pagination added to views.py function for Forum page

--- forum/views.py
+++ forum/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import HttpResponse
 from .models import Post
 
 
 # Create your views here.
 def forum(request):
+
+    forum_posts = Post.objects.order_by('-date_posted')
+
+    # Paginator
+    paginator = Paginator(forum_posts, 6)
+    page = request.GET.get('page')
+    paged_posts = paginator.get_page(page)
+
     """Render Forum page and return all available posts with pagination and
     search functionality."""
     context = {
         'forum_page': 'active',
-        'posts': Post.objects.all()
+        'posts': paged_posts
     }
 
     return render(request, 'forum/forum.html', context)
