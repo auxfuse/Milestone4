d9cdc8723f7d9fc71743ebb8f0e5fd198a4ec656
auxfuse <aux.fuse@yahoo.com>
2020-04-29 12:11:10 +0100

    Email username and password set in env.py file as variables and passed to settings.py project file for EMAIL Config

--- Milestone4/settings.py
+++ Milestone4/settings.py
@@ -159,3 +159,10 @@ STRIPE_SECRET = os.environ.get("STRIPE_SECRET")
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email Config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PW')
+EMAIL_USE_TLS = True
